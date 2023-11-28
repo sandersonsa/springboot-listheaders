ad05135f271eda46b13a2053a92e2c19024e6d3f
Sanderson Sa <sanderson@redhat.com>
2023-11-28 11:16:48 -0300

    first commit 2

--- src/main/java/xyz/sandersonsa/springbootheaders/controllers/PrincipalController.java
+++ src/main/java/xyz/sandersonsa/springbootheaders/controllers/PrincipalController.java
@@ -1,10 +1,14 @@
 package xyz.sandersonsa.springbootheaders.controllers;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,25 @@ public class PrincipalController {
 
         return new ResponseEntity<String>(
                 String.format("Listed %d headers", headers.size()), HttpStatus.OK);
+    }
+
+    @GetMapping("/multiValue")
+    public ResponseEntity<String> multiValue(
+            @RequestHeader MultiValueMap<String, String> headers) {
+        headers.forEach((key, value) -> {
+            System.out.println(String.format(
+                    "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+
+        return new ResponseEntity<String>(
+                String.format("Listed %d headers", headers.size()), HttpStatus.OK);
+    }
+
+    @GetMapping("/getBaseUrl")
+    public ResponseEntity<String> getBaseUrl(@RequestHeader HttpHeaders headers) {
+        InetSocketAddress host = headers.getHost();
+        String url = "http://" + host.getHostName() + ":" + host.getPort();
+        return new ResponseEntity<String>(String.format("Base URL = %s", url), HttpStatus.OK);
     }
 
 }
